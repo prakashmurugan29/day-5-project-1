af9327aedc5bcd3278e0c8f226e05adc35d9477d
prakashmurugan29 <prakash.bio27@gmail.com>
2024-07-09 16:16:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById('image').src="https://m.media-amazon.com/images/I/710JLg3DJ6L._AC_UF1000,1000_QL80_.jpg"});
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById('image').src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWR4P7w8QL2i23Vd--sRjtZy_AiDrp6h0Szw&s"});
